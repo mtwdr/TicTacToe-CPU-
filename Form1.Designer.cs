244e00ddddadb0bfbd86ec9f9e25f8a3b194ae19
mtwdr <matthew-ramnarine@hotmail.com>
2022-04-28 14:20:56 -0400

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Lab5_CPU_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.space0_0 = new System.Windows.Forms.Button();
+            this.space0_1 = new System.Windows.Forms.Button();
+            this.space0_2 = new System.Windows.Forms.Button();
+            this.space1_0 = new System.Windows.Forms.Button();
+            this.space1_1 = new System.Windows.Forms.Button();
+            this.space1_2 = new System.Windows.Forms.Button();
+            this.space2_0 = new System.Windows.Forms.Button();
+            this.space2_1 = new System.Windows.Forms.Button();
+            this.space2_2 = new System.Windows.Forms.Button();
+            this.outcomelabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.newgame = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TIC TAC TOE";
+            // 
+            // space0_0
+            // 
+            this.space0_0.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.space0_0.Location = new System.Drawing.Point(20, 56);
+            this.space0_0.Name = "space0_0";
+            this.space0_0.Size = new System.Drawing.Size(51, 47);
+            this.space0_0.TabIndex = 1;
+            this.space0_0.UseVisualStyleBackColor = true;
+            this.space0_0.Click += new System.EventHandler(this.space0_0_Click);
+            // 
+            // space0_1
+            // 
+            this.space0_1.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold);
+            this.space0_1.Location = new System.Drawing.Point(102, 56);
+            this.space0_1.Name = "space0_1";
+            this.space0_1.Size = new System.Drawing.Size(51, 47);
+            this.space0_1.TabIndex = 2;
+            this.space0_1.UseVisualStyleBackColor = true;
+            // 
+            // space0_2
+            // 
+            this.space0_2.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold);
+            this.space0_2.Location = new System.Drawing.Point(188, 56);
+            this.space0_2.Name = "space0_2";
+            this.space0_2.Size = new System.Drawing.Size(51, 47);
+            this.space0_2.TabIndex = 3;
+            this.space0_2.UseVisualStyleBackColor = true;
+            // 
+            // space1_0
+            // 
+            this.space1_0.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold);
+            this.space1_0.Location = new System.Drawing.Point(20, 131);
+            this.space1_0.Name = "space1_0";
+            this.space1_0.Size = new System.Drawing.Size(51, 47);
+            this.space1_0.TabIndex = 4;
+            this.space1_0.UseVisualStyleBackColor = true;
+            // 
+            // space1_1
+            // 
+            this.space1_1.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold);
+            this.space1_1.Location = new System.Drawing.Point(102, 131);
+            this.space1_1.Name = "space1_1";
+            this.space1_1.Size = new System.Drawing.Size(51, 47);
+            this.space1_1.TabIndex = 5;
+            this.space1_1.UseVisualStyleBackColor = true;
+            // 
+            // space1_2
+            // 
+            this.space1_2.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold);
+            this.space1_2.Location = new System.Drawing.Point(188, 131);
+            this.space1_2.Name = "space1_2";
+            this.space1_2.Size = new System.Drawing.Size(51, 47);
+            this.space1_2.TabIndex = 6;
+            this.space1_2.UseVisualStyleBackColor = true;
+            // 
+            // space2_0
+            // 
+            this.space2_0.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold);
+            this.space2_0.Location = new System.Drawing.Point(20, 201);
+            this.space2_0.Name = "space2_0";
+            this.space2_0.Size = new System.Drawing.Size(51, 47);
+            this.space2_0.TabIndex = 7;
+            this.space2_0.UseVisualStyleBackColor = true;
+            // 
+            // space2_1
+            // 
+            this.space2_1.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold);
+            this.space2_1.Location = new System.Drawing.Point(102, 201);
+            this.space2_1.Name = "space2_1";
+            this.space2_1.Size = new System.Drawing.Size(51, 47);
+            this.space2_1.TabIndex = 8;
+            this.space2_1.UseVisualStyleBackColor = true;
+            // 
+            // space2_2
+            // 
+            this.space2_2.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold);
+            this.space2_2.Location = new System.Drawing.Point(188, 201);
+            this.space2_2.Name = "space2_2";
+            this.space2_2.Size = new System.Drawing.Size(51, 47);
+            this.space2_2.TabIndex = 9;
+            this.space2_2.UseVisualStyleBackColor = true;
+            // 
+            // outcomelabel
+            // 
+            this.outcomelabel.AutoSize = true;
+            this.outcomelabel.Location = new System.Drawing.Point(79, 10);
+            this.outcomelabel.Name = "outcomelabel";
+            this.outcomelabel.Size = new System.Drawing.Size(0, 13);
+            this.outcomelabel.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.outcomelabel);
+            this.panel1.Location = new System.Drawing.Point(20, 254);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(219, 35);
+            this.panel1.TabIndex = 11;
+            // 
+            // newgame
+            // 
+            this.newgame.Location = new System.Drawing.Point(20, 306);
+            this.newgame.Name = "newgame";
+            this.newgame.Size = new System.Drawing.Size(75, 23);
+            this.newgame.TabIndex = 12;
+            this.newgame.Text = "New Game";
+            this.newgame.UseVisualStyleBackColor = true;
+            this.newgame.Click += new System.EventHandler(this.newgame_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(164, 309);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 13;
+            this.button11.Text = "Exit";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 344);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.newgame);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.space2_2);
+            this.Controls.Add(this.space2_1);
+            this.Controls.Add(this.space2_0);
+            this.Controls.Add(this.space1_2);
+            this.Controls.Add(this.space1_1);
+            this.Controls.Add(this.space1_0);
+            this.Controls.Add(this.space0_2);
+            this.Controls.Add(this.space0_1);
+            this.Controls.Add(this.space0_0);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button space0_0;
+        private System.Windows.Forms.Button space0_1;
+        private System.Windows.Forms.Button space0_2;
+        private System.Windows.Forms.Button space1_0;
+        private System.Windows.Forms.Button space1_1;
+        private System.Windows.Forms.Button space1_2;
+        private System.Windows.Forms.Button space2_0;
+        private System.Windows.Forms.Button space2_1;
+        private System.Windows.Forms.Button space2_2;
+        private System.Windows.Forms.Label outcomelabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button newgame;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5_CPU_
+{
+    public partial class Form1 : Form
+    {
+        int[,] space = new int[3,3];
+
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Boolean CheckXWin()
+        {
+            if(space[0,0] == 1 && space[0,1] == 1 && space[0,2] == 1 || //row wincheck
+               space[1, 0] == 1 && space[1, 1] == 1 && space[1, 2] == 1 ||
+               space[2, 0] == 1 && space[2, 1] == 1 && space[2, 2] == 1 ||
+
+               space[0, 0] == 1 && space[1, 0] == 1 && space[2, 0] == 1 || //col wincheck
+               space[0, 1] == 1 && space[1, 1] == 1 && space[2, 1] == 1 ||
+               space[0, 2] == 1 && space[1, 2] == 1 && space[2, 2] == 1 ||
+
+               space[0, 0] == 1 && space[1, 1] == 1 && space[2, 2] == 1 || //diag wincheck
+               space[0, 2] == 1 && space[1, 1] == 1 && space[2, 0] == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private Boolean CheckOWin()
+        {
+            if (space[0, 0] == 0 && space[0, 1] == 0 && space[0, 2] == 0 || //row wincheck
+               space[1, 0] == 0 && space[1, 1] == 0 && space[1, 2] == 0 ||
+               space[2, 0] == 0 && space[2, 1] == 0 && space[2, 2] == 0 ||
+
+               space[0, 0] == 0 && space[1, 0] == 0 && space[2, 0] == 0 || //col wincheck
+               space[0, 1] == 0&& space[1, 1] == 0 && space[2, 1] == 0 ||
+               space[0, 2] == 0 && space[1, 2] == 0 && space[2, 2] == 0 ||
+
+               space[0, 0] == 0 && space[1, 1] == 0 && space[2, 2] == 0 || //diag wincheck
+               space[0, 2] == 0 && space[1, 1] == 0 && space[2, 0] == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        private void newgame_Click(object sender, EventArgs e)
+        {
+
+            for(int i = 0; i < space.GetLength(0); i++)
+            {
+                for(int j = 0; j < space.GetLength(1); j++)
+                {
+                    space[i, j] = rand.Next(0, 2);
+                }
+            }
+
+            // 0 = O, 1 = X
+            //row 1
+            if (space[0,0] == 0)
+            {
+                space0_0.Text = "O";
+            } else if (space[0,0] == 1)
+            {
+                space0_0.Text = "X";
+            }
+
+            if (space[0, 1] == 0)
+            {
+                space0_1.Text = "O";
+            }
+            else if (space[0, 1] == 1)
+            {
+                space0_1.Text = "X";
+            }
+
+            if (space[0, 2] == 0)
+            {
+                space0_2.Text = "O";
+            }
+            else if (space[0, 2] == 1)
+            {
+                space0_2.Text = "X";
+            }
+
+            //row 2
+            if (space[1, 0] == 0)
+            {
+                space1_0.Text = "O";
+            }
+            else if (space[1, 0] == 1)
+            {
+                space1_0.Text = "X";
+            }
+
+            if (space[1, 1] == 0)
+            {
+                space1_1.Text = "O";
+            }
+            else if (space[1, 1] == 1)
+            {
+                space1_1.Text = "X";
+            }
+
+            if (space[1, 2] == 0)
+            {
+                space1_2.Text = "O";
+            }
+            else if (space[1, 2] == 1)
+            {
+                space1_2.Text = "X";
+            }
+
+            //row 3
+            if (space[2, 0] == 0)
+            {
+                space2_0.Text = "O";
+            }
+            else if (space[2, 0] == 1)
+            {
+                space2_0.Text = "X";
+            }
+
+            if (space[2, 1] == 0)
+            {
+                space2_1.Text = "O";
+            }
+            else if (space[2, 1] == 1)
+            {
+                space2_1.Text = "X";
+            }
+
+            if (space[2, 2] == 0)
+            {
+                space2_2.Text = "O";
+            }
+            else if (space[2, 2] == 1)
+            {
+                space2_2.Text = "X";
+            }
+
+            if (CheckOWin() == true)
+            {
+                outcomelabel.Text = "O Wins!";
+            } else if (CheckXWin() == true)
+            {
+                outcomelabel.Text = "X Wins!";
+            }
+
+            if (CheckOWin() == true && CheckXWin() == true || CheckOWin() == false && CheckXWin() == false)
+            {
+                outcomelabel.Text = "Draw";
+            }
+        }
+
+        private void space0_0_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
